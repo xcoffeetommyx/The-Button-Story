76d14a2f3b115c91b8ff82fde647a9cc57b7777d
xcoffeetommyx <xcoffeetommyx@gmail.com>
2026-06-13 18:19:01 -0500

    Make story PWA update from network

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "the-button-story-v6";
+const CACHE_NAME = "the-button-story-v7";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -32,18 +32,16 @@ self.addEventListener("fetch", (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches
+          .match(event.request)
+          .then((cached) => cached || caches.match("./index.html"))
+      )
   );
 });
